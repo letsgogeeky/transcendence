3bd681f3007249a585f5948414a38443b9574032
Borbala Pisak <bpisak-l@student.42.fr>
2025-02-12 19:52:42 +0100

    Create a token from email and use it for email address validation

--- backend/auth/db/schema.sql
+++ backend/auth/db/schema.sql
@@ -1,10 +1,13 @@
-CREATE TABLE users (
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
+    token TEXT NOT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_login TIMESTAMP,
     avatar_url TEXT DEFAULT NULL,
-    status TEXT CHECK(status IN ('pending', 'validated')) DEFAULT 'pending'
+    is_validated INTEGER DEFAULT 0
 );
