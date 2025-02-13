02dd5e8e0dda2cb56559c6a33451019a54f6db87
Borbala Pisak <bpisak-l@student.42.fr>
2025-02-13 17:01:23 +0100

    Add email verification

--- backend/auth/src/routes/register.ts
+++ backend/auth/src/routes/register.ts
@@ -1,6 +1,6 @@
-import { FastifyInstance } from 'fastify';
-
 import { Static, Type } from '@sinclair/typebox';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import nodemailer from 'nodemailer';
 import { UserType } from './users';
 
 export const RegisterDto = Type.Object({
@@ -11,11 +11,17 @@ export const RegisterDto = Type.Object({
 });
 
 export const RegisterSuccess = Type.Object({
-    success: Type.Boolean(),
     id: Type.Integer(),
 });
 
 export function authRoutes(fastify: FastifyInstance) {
+    fastify.setErrorHandler(function (error, request, reply) {
+        this.log.error(error);
+        if (error.message.includes('SQLITE_CONSTRAINT: UNIQUE constraint'))
+            error.statusCode = 409;
+        reply.status(error.statusCode || 500).send({ error: error.message });
+    });
+
     fastify.post<{ Body: Static<typeof RegisterDto> }>(
         '/register',
         {
@@ -28,41 +34,72 @@ export function authRoutes(fastify: FastifyInstance) {
         },
 
         async (request, reply) => {
-            try {
-                const { email, password, name, confirmPassword } = request.body;
-                if (confirmPassword != password) throw Error("Passwords don't match");
-                const query =
-                    'INSERT INTO users (email, password, name, token) VALUES (?, ?, ?, ?)';
-                const hash = await fastify.bcrypt.hash(password);
-                const token = fastify.jwt.sign({ email: email });
-                const result = await fastify.db.run(query, [email, hash, name, token]);
-                const response = { id: result.lastID, success: true };
-                reply.status(200).send(response);
-            } catch (error) {
-                if (error instanceof Error)
-                    reply.status(500).send({ success: false, error: error.message });
-                else reply.status(500).send({ success: false, error: 'Unknown error' });
-            }
+            const { email, password, name, confirmPassword } = request.body;
+            if (confirmPassword != password)
+                reply.status(400).send({ error: "Passwords don't match" });
+            const query =
+                'INSERT INTO users (email, password, name, token) VALUES (?, ?, ?, ?)';
+            const hash = await fastify.bcrypt.hash(password);
+            const token = fastify.jwt.sign({ email: email });
+            await sendVerificationEmail(email, token, request);
+            const result = await fastify.db.run(query, [
+                email,
+                hash,
+                name,
+                token,
+            ]);
+            reply.status(200).send({ id: result.lastID });
         },
     );
 
-    fastify.get<{ Querystring: { token: string } }>('/verify-email', async (request, reply) => {
-        try {
+    fastify.get<{ Querystring: { token: string } }>(
+        '/verify-email',
+        async (request, reply) => {
             const { token } = request.query;
             let query = 'SELECT * FROM users WHERE token = ?';
-            const user: UserType | undefined = await fastify.db.get(query, [token]);
+            const user: UserType | undefined = await fastify.db.get(query, [
+                token,
+            ]);
             if (!user)
-                return reply.code(400).send({ success: false, error: 'Invalid or expired token' });
+                return reply
+                    .code(400)
+                    .send({ error: 'Invalid or expired token' });
             const decoded = fastify.jwt.verify<{ email: string }>(token);
             if (decoded?.email != user.email)
-                return reply.code(400).send({ error: 'Invalid or expired token' });
+                return reply
+                    .code(400)
+                    .send({ error: 'Invalid or expired token' });
             query = 'UPDATE users SET is_validated = 1 WHERE id = ?';
             await fastify.db.run(query, [user.id]);
-            reply.status(200).send({ success: true, message: 'Email verified' });
+            reply.status(200).send({ message: 'Email verified' });
+        },
+    );
+
+    async function sendVerificationEmail(
+        email: string,
+        token: string,
+        req: FastifyRequest,
+    ) {
+        try {
+            const transporter: nodemailer.Transporter =
+                nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'noreply.transcendence2025@gmail.com',
+                        pass: fastify.config.GOOGLE_PASS,
+                    },
+                });
+            console.log(req.hostname);
+            const tokenLink = `<a href = "${req.protocol}://${req.hostname}:${req.port}/verify-email?token=${token}&email=${email}"> Email verification </a>`;
+            console.log(tokenLink);
+            await transporter.sendMail({
+                from: '"noreply transcendence" <noreply.transcendence2025@gmail.com>',
+                to: email,
+                subject: 'Verify your email',
+                html: '<b>Click the link to verify your email?</b>' + tokenLink, // html body
+            });
         } catch (error) {
-            if (error instanceof Error)
-                reply.status(500).send({ success: false, error: error.message });
-            else reply.status(500).send({ success: false, error: 'Unknown error' });
+            throw Error('Error sending email:' + (error as Error).message);
         }
-    });
+    }
 }
