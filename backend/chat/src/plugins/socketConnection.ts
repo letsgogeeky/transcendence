3176cd255a88fc25eb146eb6d2c938f7ea1bf4b3
Ramy Moussa <ramymohamedmousa@gmail.com>
2025-04-09 09:10:20 +0200

    Integrated chat backend app into environment

--- backend/chat/src/plugins/socketConnection.ts
+++ backend/chat/src/plugins/socketConnection.ts
@@ -4,8 +4,8 @@ import { FastifyPluginCallback } from "fastify";
 import fp from "fastify-plugin";
 import { WebSocket } from "ws";
 const socketConnectionPlugin: FastifyPluginCallback = fp((server, _options) => {
-    const chatConnections = new Map<string, WebSocket>();
-    server.decorate('chatConnections', chatConnections);
+    const connections = new Map<string, WebSocket>();
+    server.decorate('connections', connections);
 });
 
 export default socketConnectionPlugin;
